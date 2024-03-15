24d630ba68dea5f42717d90731fcde172f2c5323
ArthurOrlov <81583856+ArthurOrlov@users.noreply.github.com>
2024-03-15 17:31:37 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.burger').addEventListener('click', function(){
-    document.querySelector('#menu').classList.toggle('is-active')
-})
-
-document.querySelector('.burger__ws-menu').addEventListener('click', function(){
-    document.querySelector('#menu__ws-menu').classList.toggle('is-active')
-})

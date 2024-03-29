1c12f7f8961bc8e0c5e7920af2f8997783ac9e40
ArthurOrlov <81583856+ArthurOrlov@users.noreply.github.com>
2024-03-29 17:17:23 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.nav__link').forEach(function(tabsBtn) {
-        tabsBtn.addEventListener('click', function(event) {
-            const path = event.currentTarget.dataset.path
-
-            document.querySelectorAll('.tab-content').forEach(function(tabContent) {
-                tabContent.classList.remove('tab-content-active')
-            })
-            document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
-        })
-    })
-})
-
 document.querySelector('.burger').addEventListener('click', function(){
     document.querySelector('#menu').classList.toggle('is-active')
 })
